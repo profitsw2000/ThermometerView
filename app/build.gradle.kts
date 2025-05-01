496c90be5f736a478032bf12a18fb3e1a4d0a3db
profitsw2000 <profitsw2000@gmail.com>
2025-05-01 15:23:18 +0300

    Сделана настройка многомодульности - созданы плагины gradle для модулей разного типа.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,38 +1,9 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.tv.main.app.gradle.plugin)
 }
 
 android {
     namespace = "ru.profitsw2000.thermometerview"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "ru.profitsw2000.thermometerview"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
