be356cf412e25ae8100bb546732d209316a338d1
profitsw2000 <profitsw2000@gmail.com>
2025-05-04 20:51:49 +0300

    Определены основные цвета в используемой теме приложения.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,30 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.tv.main.app.gradle.plugin)
 }
 
 android {
     namespace = "ru.profitsw2000.thermometerview"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "ru.profitsw2000.thermometerview"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+    //Modules
+    implementation(project(":core"))
+    implementation(project(":data"))
+    implementation(project(":mainFragment"))
+    implementation(project(":tableTab"))
+    implementation(project(":graphTab"))
+    implementation(project(":navigator"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    //Koin
+    implementation(libs.koin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/ru/profitsw2000/thermometerview/App.kt
@@ -0,0 +1,16 @@
+package ru.profitsw2000.thermometerview
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+        }
+    }
+}
