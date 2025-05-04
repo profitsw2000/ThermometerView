1f0e35e976a8633d4d58d82660a08ac8c506033e
profitsw2000 <profitsw2000@gmail.com>
2025-05-04 09:33:37 +0300

    Создан модуль с главным экраном.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,17 @@ dependencies {
     //Modules
     implementation(project(":core"))
     implementation(project(":data"))
+    implementation(project(":mainFragment"))
+    implementation(project(":tableTab"))
+    implementation(project(":graphTab"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ mainFragment/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.tv.main.lib.gradle.plugin)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "ru.profitsw2000.mainfragment"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
