72b73eaaccef6a52e3c76ec7c4d4041f60c4c8f2
profitsw2000 <profitsw2000@gmail.com>
2025-05-17 21:38:38 +0300

    Создана вьюмодель для нижнего всплывающего диалогового фрагмента, строки и размеры убраны в ресурсы.

--- mainFragment/src/main/java/ru/profitsw2000/mainfragment/di/modules.kt
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/di/modules.kt
@@ -5,8 +5,10 @@ import org.koin.dsl.module
 import ru.profitsw2000.data.data.BluetoothRepositoryImpl
 import ru.profitsw2000.data.domain.BluetoothRepository
 import ru.profitsw2000.mainfragment.presentation.viewmodel.MainViewModel
+import ru.profitsw2000.mainfragment.presentation.viewmodel.PairedDevicesViewModel
 
 val mainModule = module {
     single<BluetoothRepository> { BluetoothRepositoryImpl(androidContext()) }
     single { MainViewModel(get()) }
+    single { PairedDevicesViewModel(get()) }
 }
--- mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/BluetoothPairedDevicesListFragment.kt
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/BluetoothPairedDevicesListFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import ru.profitsw2000.mainfragment.R
 
-class BluetoothPairedDevicesListFragment : Fragment() {
+class BluetoothPairedDevicesListFragment : BottomSheetDialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
