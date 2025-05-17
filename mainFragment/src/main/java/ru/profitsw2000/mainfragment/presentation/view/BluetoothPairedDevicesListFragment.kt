e87e587495d2e7b9c8b33c4550cd910b34eda921
profitsw2000 <profitsw2000@gmail.com>
2025-05-17 21:23:55 +0300

    Сделан макет нижнего всплывающего диалогового окна.

---
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/BluetoothPairedDevicesListFragment.kt
@@ -0,0 +1,28 @@
+package ru.profitsw2000.mainfragment.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.profitsw2000.mainfragment.R
+
+class BluetoothPairedDevicesListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bluetooth_paired_devices_list, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = BluetoothPairedDevicesListFragment()
+    }
+}
