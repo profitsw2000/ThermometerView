17b20b5b153de6e21c43499eb1598219c92b6578
profitsw2000 <profitsw2000@gmail.com>
2025-08-13 22:16:15 +0300

    В интерфейс навигатора и его реализацию добавлен метод для перехода на нижнее всплывающее диалоговое окно с информацией о датчике.

--- app/src/main/java/ru/profitsw2000/thermometerview/navigation/NavigatorImpl.kt
+++ app/src/main/java/ru/profitsw2000/thermometerview/navigation/NavigatorImpl.kt
@@ -1,7 +1,12 @@
 package ru.profitsw2000.thermometerview.navigation
 
+import android.os.Bundle
 import androidx.navigation.NavController
+import ru.profitsw2000.core.R
 import ru.profitsw2000.navigator.Navigator
 
 class NavigatorImpl(private val navController: NavController) : Navigator {
+    override fun navigateToSensorInfoBottomSheet(bundle: Bundle) {
+        navController.navigate(ru.profitsw2000.thermometerview.R.id.action_main_to_sensor_info, bundle)
+    }
 }
