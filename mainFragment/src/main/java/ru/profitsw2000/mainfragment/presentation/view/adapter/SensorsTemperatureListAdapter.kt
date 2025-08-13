6fc219857ca98106a2522ccc9716fd4dbe7e428a
profitsw2000 <profitsw2000@gmail.com>
2025-08-13 22:32:59 +0300

    В конструктор адаптера SensorsTemperatureListAdapter добавлена переменная типа OnSensorItemClickListener для определения индекса нажатого элемента из списка с информацией о датчиках.

--- mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/adapter/SensorsTemperatureListAdapter.kt
+++ mainFragment/src/main/java/ru/profitsw2000/mainfragment/presentation/view/adapter/SensorsTemperatureListAdapter.kt
@@ -3,11 +3,14 @@ package ru.profitsw2000.mainfragment.presentation.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import ru.profitsw2000.core.utils.listeners.OnSensorItemClickListener
 import ru.profitsw2000.data.model.SensorModel
 import ru.profitsw2000.mainfragment.databinding.SensorsTemperatureRvItemBinding
 import java.util.Locale
 
-class SensorsTemperatureListAdapter : RecyclerView.Adapter<SensorsTemperatureListAdapter.ViewHolder>() {
+class SensorsTemperatureListAdapter(
+    private val onSensorItemClickListener: OnSensorItemClickListener
+) : RecyclerView.Adapter<SensorsTemperatureListAdapter.ViewHolder>() {
 
     private var data: List<SensorModel> = arrayListOf()
 
@@ -21,8 +24,17 @@ class SensorsTemperatureListAdapter : RecyclerView.Adapter<SensorsTemperatureLis
         viewType: Int
     ): SensorsTemperatureListAdapter.ViewHolder {
         val binding = SensorsTemperatureRvItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val sensorInfoViewHolder = ViewHolder(binding)
 
-        return ViewHolder(binding)
+        with(binding) {
+            root.setOnClickListener {
+                onSensorItemClickListener.onClick(
+                    sensorInfoViewHolder.adapterPosition
+                )
+            }
+        }
+
+        return sensorInfoViewHolder
     }
 
     override fun onBindViewHolder(holder: SensorsTemperatureListAdapter.ViewHolder, position: Int) {
