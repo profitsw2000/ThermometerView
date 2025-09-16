0d2430b25661bb902b2e104c9750391a83613c91
profitsw2000 <profitsw2000@gmail.com>
2025-09-16 21:44:21 +0300

    В модуле дата cоздан пакет room с подпакетами для работы с БД, создан класс-таблица для хранения данных с историей измеренной температуры.

---
+++ data/src/main/java/ru/profitsw2000/data/room/entity/SensorHistoryDataEntity.kt
@@ -0,0 +1,16 @@
+package ru.profitsw2000.data.room.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity
+data class SensorHistoryDataEntity(
+    @PrimaryKey
+    val id: ULong,
+    val localId: Int,
+    val sensorId: ULong,
+    val letterCode: Int,
+    val date: Date,
+    val temperature: Double
+)
