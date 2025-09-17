ae20390f1e6ac0095423460d7bee7a0c1b2a6131
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-09-17 13:44:26 +0300

    Сделан класс с БД Room и интерфейс с функциями для работы с этой БД.

---
+++ data/src/main/java/ru/profitsw2000/data/room/dao/SensorHistoryDao.kt
@@ -0,0 +1,18 @@
+package ru.profitsw2000.data.room.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.profitsw2000.data.room.entity.SensorHistoryDataEntity
+
+@Dao
+interface SensorHistoryDao {
+
+    @Query("SELECT * FROM SensorHistoryDataEntity " +
+            "ORDER BY SensorHistoryDataEntity.date DESC LIMIT :limit OFFSET :offset")
+    suspend fun getSensorHistoryList(limit: Int, offset: Int)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(sensorHistoryDataEntityList: List<SensorHistoryDataEntity>)
+}
