b9a51d7f9a368a8cbc29520712b7c3c0ea4e4a96
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-09-19 14:42:42 +0300

    Сделан класс для хранения состояния при работе с памятью термометра.

---
+++ data/src/main/java/ru/profitsw2000/data/model/state/MemoryScreenState.kt
@@ -0,0 +1,16 @@
+package ru.profitsw2000.data.model.state
+
+import ru.profitsw2000.data.model.MemoryInfoModel
+
+sealed class MemoryScreenState {
+    data object Blank: MemoryScreenState()
+    data object MemoryInfoLoad: MemoryScreenState()
+    data class MemoryInfoSuccess(val memoryInfoModel: MemoryInfoModel): MemoryScreenState()
+    data object MemoryClearExecution: MemoryScreenState()
+    data object MemoryClearSuccess: MemoryScreenState()
+    data class ServiceDataRequest(val message: String): MemoryScreenState()
+    data class ServiceDataAnswer(val message: String): MemoryScreenState()
+    data class MemoryDataRequest(val message: String, val percentProgress: Int): MemoryScreenState()
+    data class MemoryDataAnswer(val message: String, val percentProgress: Int): MemoryScreenState()
+    data object MemoryDataSuccess: MemoryScreenState()
+}
