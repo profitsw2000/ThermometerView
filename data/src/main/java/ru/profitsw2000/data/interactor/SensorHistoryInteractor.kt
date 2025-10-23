e42c8b51416c5605c064aa3bec4555793bf6907b
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-10-23 13:57:39 +0300

    В интерактор добавлены функции записи в базу данных одной и нескольких записей данных датчиков.

--- data/src/main/java/ru/profitsw2000/data/interactor/SensorHistoryInteractor.kt
+++ data/src/main/java/ru/profitsw2000/data/interactor/SensorHistoryInteractor.kt
@@ -1,4 +1,20 @@
 package ru.profitsw2000.data.interactor
 
-class SensorHistoryInteractor {
+import ru.profitsw2000.data.domain.local.SensorHistoryRepositoryLocal
+import ru.profitsw2000.data.domain.remote.SensorHistoryRepositoryRemote
+import ru.profitsw2000.data.room.entity.SensorHistoryDataEntity
+
+class SensorHistoryInteractor(
+    val sensorHistoryRepositoryLocal: SensorHistoryRepositoryLocal,
+    val sensorHistoryRepositoryRemote: SensorHistoryRepositoryRemote
+) {
+
+    suspend fun writeHistoryItem(
+        sensorHistoryDataEntity: SensorHistoryDataEntity,
+        isRemote: Boolean
+    ) {
+        if (isRemote) sensorHistoryRepositoryRemote.writeHistoryItem(sensorHistoryDataEntity = sensorHistoryDataEntity)
+        else sensorHistoryRepositoryLocal.writeHistoryItem(sensorHistoryDataEntity = sensorHistoryDataEntity)
+    }
+
 }
