ed854324b7c9d6e2eb76d6fdb686188ef144c658
profitsw2000 <profitsw2000@gmail.com>
2025-11-05 21:59:20 +0300

    Добавление в проект класса HistoryListPagingSource.kt - источника данных для списка с историей изменения температуры.

---
+++ data/src/main/java/ru/profitsw2000/data/data/local/source/HistoryListPagingSource.kt
@@ -0,0 +1,36 @@
+package ru.profitsw2000.data.data.local.source
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import ru.profitsw2000.data.mappers.SensorHistoryMapper
+import ru.profitsw2000.data.model.SensorHistoryDataModel
+import ru.profitsw2000.data.room.dao.SensorHistoryDao
+
+class HistoryListPagingSource(
+    private val sensorHistoryDao: SensorHistoryDao,
+    private val sensorHistoryMapper: SensorHistoryMapper
+) : PagingSource<Int, SensorHistoryDataModel>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SensorHistoryDataModel> {
+        val page = params.key ?: 0
+
+        return try {
+            val sensorHistoryListPage = sensorHistoryDao.getSensorHistoryList(params.loadSize, page*params.loadSize)
+
+            LoadResult.Page(
+                data = sensorHistoryMapper.map(sensorHistoryListPage),
+                prevKey = if (page == 0) null else page - 1,
+                nextKey = if (sensorHistoryListPage.isEmpty()) null else page + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, SensorHistoryDataModel>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+}
