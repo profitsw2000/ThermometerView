c7cd2ab368cde5c4825a0c7795f55acf52e58df2
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-11-06 15:08:28 +0300

    Создана TableViewModel.kt для работы с экраном табличного отображения истории изменения температуры.

---
+++ tableTab/src/main/java/ru/profitsw2000/tabletab/presentation/viewmodel/TableViewModel.kt
@@ -0,0 +1,11 @@
+package ru.profitsw2000.tabletab.presentation.viewmodel
+
+import ru.profitsw2000.data.interactor.SensorHistoryInteractor
+
+class TableViewModel(
+    private val sensorHistoryInteractor: SensorHistoryInteractor
+) {
+
+
+
+}
