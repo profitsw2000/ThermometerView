98a3fc78ffd0df54f54cc48430e9ffe80de19a09
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-11-07 11:23:25 +0300

    В TableViewModel.kt добавлен источник живых данных с постраничными данными с историей из памяти термометра.

--- tableTab/src/main/java/ru/profitsw2000/tabletab/presentation/viewmodel/TableViewModel.kt
+++ tableTab/src/main/java/ru/profitsw2000/tabletab/presentation/viewmodel/TableViewModel.kt
@@ -1,11 +1,20 @@
 package ru.profitsw2000.tabletab.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import ru.profitsw2000.data.interactor.SensorHistoryInteractor
+import ru.profitsw2000.data.model.SensorHistoryDataModel
 
 class TableViewModel(
     private val sensorHistoryInteractor: SensorHistoryInteractor
-) {
+) : ViewModel() {
 
-
+    val historyListPagedData: LiveData<PagingData<SensorHistoryDataModel>> =
+        sensorHistoryInteractor
+            .getHistoryPagedData(false)
+            .cachedIn(viewModelScope)
 
 }
