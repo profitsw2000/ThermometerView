9deb638639584dc50e9899ea6b1723ef16f85b73
profitsw2000 <profitsw2000@gmail.com>
2025-11-10 22:13:26 +0300

    Сделано меню для экрана со спискомистории изменения температуры.

--- tableTab/src/main/java/ru/profitsw2000/tabletab/presentation/view/TableFragment.kt
+++ tableTab/src/main/java/ru/profitsw2000/tabletab/presentation/view/TableFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
@@ -31,6 +34,11 @@ class TableFragment : Fragment() {
         SensorHistoryListAdapter()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,6 +52,27 @@ class TableFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initViews()
         observeData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.table_tab_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.time_period_selection -> {
+                true
+            }
+            R.id.filter_history -> {
+                true
+            }
+            R.id.sort_history -> {
+                true
+            }
+            else -> true
+        }
     }
 
     private fun initViews() = with(binding) {
