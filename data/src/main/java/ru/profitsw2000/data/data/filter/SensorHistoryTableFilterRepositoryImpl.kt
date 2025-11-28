6fab00cbba10c7e0a8884dd9caf19b53e444fc8f
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-11-28 14:20:17 +0300

    Сделан файл с константами, содержащими коэффициенты для соответствующих временных периодов, которые будут использоваться с целью вычисления температуры в выбранный промежуток времени выбранным на экране фильтрации способом.

--- data/src/main/java/ru/profitsw2000/data/data/filter/SensorHistoryTableFilterRepositoryImpl.kt
+++ data/src/main/java/ru/profitsw2000/data/data/filter/SensorHistoryTableFilterRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package ru.profitsw2000.data.data.filter
 
 import ru.profitsw2000.data.domain.filter.SensorHistoryTableFilterRepository
+import ru.profitsw2000.data.enumer.TimeFrameDataObtainingMethod
 import java.util.Date
 
 class SensorHistoryTableFilterRepositoryImpl : SensorHistoryTableFilterRepository {
@@ -8,6 +9,7 @@ class SensorHistoryTableFilterRepositoryImpl : SensorHistoryTableFilterRepositor
     override var localIdList: List<Int> = arrayListOf()
     override var letterCodeList: List<Int> = arrayListOf()
     override var timeFrameFactor: Int = 1
+    override var timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod = TimeFrameDataObtainingMethod.TimeFrameEnd
     override var isAscendingOreder: Boolean = true
     override var fromDate: Date? = null
     override var toDate: Date? = null
--- data/src/main/java/ru/profitsw2000/data/domain/filter/SensorHistoryTableFilterRepository.kt
+++ data/src/main/java/ru/profitsw2000/data/domain/filter/SensorHistoryTableFilterRepository.kt
@@ -1,9 +1,12 @@
 package ru.profitsw2000.data.domain.filter
 
+import ru.profitsw2000.data.enumer.TimeFrameDataObtainingMethod
+
 interface SensorHistoryTableFilterRepository : SensorHistoryFilterRepository {
     var sensorIdList: List<Long>
     var localIdList: List<Int>
     var letterCodeList: List<Int>
     var timeFrameFactor: Int
+    var timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod
     var isAscendingOreder: Boolean
 }
