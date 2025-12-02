4cdc6a51e4478ba8d1ca529e18876ff9b7ebd4f6
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-12-02 15:04:02 +0300

    Сделана дата класс для хранения промежуточных данных с информацией об изменении температуры и добавлена функция в SensorHistoryMapper для преобразования списка с учетом фильтров.

--- data/src/main/java/ru/profitsw2000/data/mappers/SensorHistoryMapper.kt
+++ data/src/main/java/ru/profitsw2000/data/mappers/SensorHistoryMapper.kt
@@ -1,7 +1,11 @@
 package ru.profitsw2000.data.mappers
 
+import ru.profitsw2000.core.utils.constants.TEN_MINUTES_FRAME_FACTOR
+import ru.profitsw2000.data.enumer.TimeFrameDataObtainingMethod
 import ru.profitsw2000.data.model.SensorHistoryDataModel
+import ru.profitsw2000.data.model.SensorHistoryTimeFrameDataModel
 import ru.profitsw2000.data.room.entity.SensorHistoryDataEntity
+import java.util.Date
 
 class SensorHistoryMapper {
 
@@ -38,4 +42,85 @@ class SensorHistoryMapper {
         return sensorHistoryDataEntityList.map { map(it) }
     }
 
+    fun filteredMap(
+        sensorHistoryDataEntityList: List<SensorHistoryDataEntity>,
+        timeFrameFactor: Int,
+        timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod,
+        isAscendingOrder: Boolean
+    ): List<SensorHistoryDataModel> {
+        return if (timeFrameFactor == TEN_MINUTES_FRAME_FACTOR)
+            map(sensorHistoryDataEntityList = sensorHistoryDataEntityList)
+        else
+            getFilteredSensorHistoryList(
+                sensorHistoryDataEntityList = sensorHistoryDataEntityList,
+                timeFrameFactor = timeFrameFactor,
+                timeFrameDataObtainingMethod = timeFrameDataObtainingMethod,
+                isAscendingOrder = isAscendingOrder
+            )
+    }
+
+    private fun getFilteredSensorHistoryList(
+        sensorHistoryDataEntityList: List<SensorHistoryDataEntity>,
+        timeFrameFactor: Int,
+        timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod,
+        isAscendingOrder: Boolean
+    ): List<SensorHistoryDataModel> {
+        val sensorHistoryTimeFrameDataModelList = mutableListOf<SensorHistoryTimeFrameDataModel>()
+        val sensorHistoryDataModelMutableList = mutableListOf<SensorHistoryDataModel>()
+
+        sensorHistoryDataEntityList.forEach { item ->
+            val sensorIndexInList = getIndexOfElementInSensorHistoryTimeFrameDataModelListBySerialNumber(
+                sensorHistoryTimeFrameDataModelList,
+                item.sensorId
+            )
+
+            if (sensorIndexInList != -1) {
+                val isInRange = isDateInRange(
+                    date = item.date,
+                    timeFrameEnd = sensorHistoryTimeFrameDataModelList[sensorIndexInList].timeFrameEndDate,
+                    timeFrameStart = sensorHistoryTimeFrameDataModelList[sensorIndexInList].timeFrameStartDate,
+                    isAscendingDate = isAscendingOrder
+                )
+                if (isInRange)
+                    sensorHistoryTimeFrameDataModelList[sensorIndexInList].temperatureMutableList.add(item.temperature)
+                else
+                    calculateTemperatureAndUpdateFrame()
+            }
+        }
+    }
+
+    private fun getIndexOfElementInSensorHistoryTimeFrameDataModelListBySerialNumber(
+        sensorHistoryTimeFrameDataModelList: List<SensorHistoryTimeFrameDataModel>,
+        sensorId: Long
+    ): Int {
+        sensorHistoryTimeFrameDataModelList.forEachIndexed { index, item ->
+            if (item.sensorHistoryDataModel.sensorId == sensorId) return index
+        }
+        return -1
+    }
+
+    private fun isDateInRange(
+        date: Date,
+        timeFrameEnd: Date,
+        timeFrameStart: Date,
+        isAscendingDate: Boolean
+    ): Boolean {
+        return if (isAscendingDate)
+            (date.after(timeFrameStart) || date == timeFrameStart) && date.before(timeFrameEnd)
+        else
+            (date.before(timeFrameStart) || date == timeFrameStart) && date.after(timeFrameEnd)
+    }
+
+    private fun getTimeFrameEndDate(
+        timeFrameStart: Date,
+        timeFrameFactor: Int,
+        isAscendingDate: Boolean
+    ): Date {
+        val timeFrameInMillis = if (isAscendingDate) (timeFrameFactor*10*60*1000L)
+        else -(timeFrameFactor*10*60*1000L)
+
+        return Date(timeFrameStart.time + timeFrameInMillis)
+    }
+
+    private fun calculateTemperatureAndUpdateFrame() {}
 }
