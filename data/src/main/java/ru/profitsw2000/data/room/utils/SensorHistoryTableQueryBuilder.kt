e3ac3bdb5aa20a9407c573dfc9c8ff114571c3af
SpectrumAnalyzer <profitsw2000@gmail.com>
2025-12-10 15:01:56 +0300

    В SensorHistoryTableQueryBuilder сделаны строки запросов.

--- data/src/main/java/ru/profitsw2000/data/room/utils/SensorHistoryTableQueryBuilder.kt
+++ data/src/main/java/ru/profitsw2000/data/room/utils/SensorHistoryTableQueryBuilder.kt
@@ -1,17 +1,74 @@
 package ru.profitsw2000.data.room.utils
 
+import ru.profitsw2000.core.utils.constants.TEN_MINUTES_FRAME_MILLIS
 import ru.profitsw2000.data.domain.filter.SensorHistoryTableFilterRepository
+import ru.profitsw2000.data.enumer.TimeFrameDataObtainingMethod
 
 class SensorHistoryTableQueryBuilder(
     sensorHistoryTableFilterRepository: SensorHistoryTableFilterRepository
 ) {
 
-    private val fromPart = "FROM SensorHistoryDataEntity "
-    private val dateRangePart = "WHERE date BETWEEN ? AND ? "
+    private val dateRangePart = "WHERE (date BETWEEN ? AND ?) "
     private val fullListOrderByPart = "ORDER BY " +
             "CASE WHEN ? THEN SensorHistoryDataEntity.date END ASC, " +
             "CASE WHEN NOT ? THEN SensorHistoryDataEntity.date END DESC "
     private val endPart = "LIMIT ? OFFSET ?"
+    private val querySelectPart = getQuerySelectPart(
+        sensorHistoryTableFilterRepository.timeFrameMillis,
+        sensorHistoryTableFilterRepository.timeFrameDataObtainingMethod
+    )
+    private val dataFilterPart = "WHERE (date BETWEEN ? AND ?) ${getAdditionalConditionToDataFilterPart(
+        sensorHistoryTableFilterRepository.sensorIdList,
+        sensorHistoryTableFilterRepository.localIdList,
+        sensorHistoryTableFilterRepository.letterCodeList
+    )}"
+    private val queryGroupByPart = "GROUP BY sensorId, date/? "
+    private val args = mutableListOf<Any>()
 
 
+    private fun getQuerySelectPart(
+        timeFrameMillis: Long,
+        timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod
+    ): String {
+        return if (timeFrameMillis == TEN_MINUTES_FRAME_MILLIS) {
+            "SELECT * "
+        } else {
+            getTimeFrameQuerySelectPart(timeFrameDataObtainingMethod)
+        }
+    }
+
+    private fun getTimeFrameQuerySelectPart(timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod): String {
+        return "SELECT ${getTemperatureValueSqlStringOperator(timeFrameDataObtainingMethod)} " +
+                "AS temperature, ${getDateAndIdValueSqlStringOperator(timeFrameDataObtainingMethod)} " +
+                "sensorId, localId, letterCode FROM SensorHistoryDataEntity "
+    }
+
+    private fun getTemperatureValueSqlStringOperator(timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod): String {
+        return when(timeFrameDataObtainingMethod) {
+            TimeFrameDataObtainingMethod.TimeFrameAverage -> "AVG(temperature)"
+            TimeFrameDataObtainingMethod.TimeFrameMaximum -> "MAX(temperature)"
+            TimeFrameDataObtainingMethod.TimeFrameMinimum -> "MIN(temperature)"
+            else -> "DISTINCT FIRST_VALUE(temperature) OVER w"
+        }
+    }
+
+    private fun getDateAndIdValueSqlStringOperator(timeFrameDataObtainingMethod: TimeFrameDataObtainingMethod): String {
+        return if (timeFrameDataObtainingMethod == TimeFrameDataObtainingMethod.TimeFrameAverage ||
+            timeFrameDataObtainingMethod == TimeFrameDataObtainingMethod.TimeFrameMaximum ||
+            timeFrameDataObtainingMethod == TimeFrameDataObtainingMethod.TimeFrameMinimum)
+            "MIN(date) AS date, MIN(id) AS id,"
+        else "MIN(date) OVER w AS date, id,"
+    }
+
+    private fun getAdditionalConditionToDataFilterPart(
+        sensorIdList: List<Long>,
+        localIdList: List<Int>,
+        letterCodeList: List<Int>
+    ): String {
+        return if (sensorIdList.isNotEmpty() ||
+            localIdList.isNotEmpty() ||
+            letterCodeList.isNotEmpty())
+            "AND (sensorId IN ? OR localId IN ? OR letterCode IN ?) "
+        else ""
+    }
 }
