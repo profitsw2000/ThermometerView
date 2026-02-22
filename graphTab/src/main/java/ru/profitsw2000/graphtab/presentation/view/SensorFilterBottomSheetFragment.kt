53cfa2086f4fc56a926b66554b00415655786155
profitsw2000 <profitsw2000@gmail.com>
2026-02-22 12:36:36 +0300

    Доработан макет фрагмента с выбором датчиков для отображения их истории изменения температуры на графике.

--- graphTab/src/main/java/ru/profitsw2000/graphtab/presentation/view/SensorFilterBottomSheetFragment.kt
+++ graphTab/src/main/java/ru/profitsw2000/graphtab/presentation/view/SensorFilterBottomSheetFragment.kt
@@ -6,16 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
+import ru.profitsw2000.data.model.state.sensorfilterscreen.LetterCodesLoadState
+import ru.profitsw2000.data.model.state.sensorfilterscreen.SensorIdsLoadState
 import ru.profitsw2000.graphtab.R
 import ru.profitsw2000.graphtab.databinding.FragmentSensorFilterBottomSheetDialogBinding
+import ru.profitsw2000.graphtab.presentation.viewmodel.GraphViewModel
 
 class SensorFilterBottomSheetFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentSensorFilterBottomSheetDialogBinding? = null
     private val binding
         get() = _binding!!
+    private val graphViewModel: GraphViewModel by activityViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,4 +46,37 @@ class SensorFilterBottomSheetFragment : BottomSheetDialogFragment() {
         val layout: CoordinatorLayout = binding.rootCoordinatorLayout
         layout.minimumHeight = 500
     }
+
+    private fun observeData() {
+        observeSensorIdData()
+        observeLetterCodeData()
+    }
+
+    private fun observeSensorIdData() {
+        val observer = Observer<SensorIdsLoadState> { renderSensorIdData(it) }
+        graphViewModel.sensorIdsListLiveData.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun observeLetterCodeData() {
+        val observer = Observer<LetterCodesLoadState> { renderLetterCodeData(it) }
+        graphViewModel.letterCodesListLiveData.observe(viewLifecycleOwner, observer)
+    }
+
+    private fun renderSensorIdData(sensorIdsLoadState: SensorIdsLoadState) {
+        when(sensorIdsLoadState) {
+            SensorIdsLoadState.Error -> TODO()
+            SensorIdsLoadState.Loading -> TODO()
+            is SensorIdsLoadState.Success -> TODO()
+        }
+    }
+
+    private fun renderLetterCodeData(letterCodesLoadState: LetterCodesLoadState) {
+        when(letterCodesLoadState) {
+            LetterCodesLoadState.Error -> TODO()
+            LetterCodesLoadState.Loading -> TODO()
+            is LetterCodesLoadState.Success -> TODO()
+        }
+    }
+
+    private fun handle
 }
