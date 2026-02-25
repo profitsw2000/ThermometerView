24716b1d37e74e35247ffbc7465be8d9dbf443b4
SpectrumAnalyzer <profitsw2000@gmail.com>
2026-02-25 16:52:23 +0300

    Класс SensorHistoryTableQueryBuilder.kt для формирования строки SQL запроса - начало.

--- data/src/main/java/ru/profitsw2000/data/room/utils/SensorHistoryTableQueryBuilder.kt
+++ data/src/main/java/ru/profitsw2000/data/room/utils/SensorHistoryTableQueryBuilder.kt
@@ -4,10 +4,30 @@ import ru.profitsw2000.core.utils.constants.TEN_MINUTES_FRAME_MILLIS
 import ru.profitsw2000.data.domain.filter.SensorHistoryTableFilterRepository
 import ru.profitsw2000.data.enumer.TimeFrameDataObtainingMethod
 
+private const val SELECT_KEY_WORD = "SELECT "
+private const val FROM_KEY_WORD = "FROM "
+private const val WHERE_KEY_WORD = "WHERE "
+private const val AND_KEY_WORD = "AND "
+private const val GROUP_KEY_WORD = "GROUP "
+private const val ORDER_KEY_WORD = "ORDER "
+private const val FIRST_QUERY = 1
+private const val SECOND_QUERY = 2
+private const val THIRD_QUERY = 3
+private const val FOURTH_QUERY = 4
+private const val FIFTH_QUERY = 5
+private const val SIXTH_QUERY = 6
+private const val SEVENTH_QUERY = 7
+private const val EIGHTH_QUERY = 8
+private const val NINTH_QUERY = 9
+private const val TENTH_QUERY = 10
+
 class SensorHistoryTableQueryBuilder(
-    sensorHistoryTableFilterRepository: SensorHistoryTableFilterRepository
+    val sensorHistoryTableFilterRepository: SensorHistoryTableFilterRepository
 ) {
 
+
+    //SELECT PART
+    private val selectPart = "$SELECT_KEY_WORD ${}"
     private val dateRangePart = "WHERE (date BETWEEN ? AND ?) "
     private val fullListOrderByPart = "ORDER BY " +
             "CASE WHEN ? THEN SensorHistoryDataEntity.date END ASC, " +
@@ -71,4 +91,78 @@ class SensorHistoryTableQueryBuilder(
             "AND (sensorId IN ? OR localId IN ? OR letterCode IN ?) "
         else ""
     }
+
+    ///////////////////////////////////////////////////////////////////////
+    //////////// QUERY NUMBER /////////////////////////////////////////////
+    private fun getQueryNumber(): Int {
+        return if (sensorHistoryTableFilterRepository.sensorIdList.isEmpty()
+            && sensorHistoryTableFilterRepository.localIdList.isEmpty()
+            && sensorHistoryTableFilterRepository.letterCodeList.isEmpty()) {
+            getQueryNumberWithoutMainFilters()
+        }
+        else getFilteredQueryNumber()
+    }
+
+    private fun getQueryNumberWithoutMainFilters(): Int {
+        return if (sensorHistoryTableFilterRepository.timeFrameMillis == TEN_MINUTES_FRAME_MILLIS)
+            FIRST_QUERY
+        else getWithoutMainFiltersTimeFrameQueryNumber()
+    }
+
+    private fun getFilteredQueryNumber(): Int {
+        return if (sensorHistoryTableFilterRepository.timeFrameMillis == TEN_MINUTES_FRAME_MILLIS)
+            SECOND_QUERY
+        else getFilteredTimeFrameQueryNumber()
+    }
+
+    private fun getWithoutMainFiltersTimeFrameQueryNumber(): Int {
+        return when(sensorHistoryTableFilterRepository.timeFrameDataObtainingMethod) {
+            TimeFrameDataObtainingMethod.TimeFrameAverage -> SEVENTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameBegin -> TENTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameEnd -> TENTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameMaximum -> EIGHTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameMinimum -> NINTH_QUERY
+        }
+    }
+
+    private fun getFilteredTimeFrameQueryNumber(): Int {
+        return when(sensorHistoryTableFilterRepository.timeFrameDataObtainingMethod) {
+            TimeFrameDataObtainingMethod.TimeFrameAverage -> THIRD_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameBegin -> SIXTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameEnd -> SIXTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameMaximum -> FOURTH_QUERY
+            TimeFrameDataObtainingMethod.TimeFrameMinimum -> FIFTH_QUERY
+        }
+    }
+
+    //QUERY STRINGS
+    private fun getQuerySelectPartCore(): String {
+        val queryNumber = getQueryNumber()
+        val pseudonymsStringQuery = "MIN(id) AS id, sensorId, localId, letterCode, MIN(date) AS date "
+        val pseudonymsStringWindowQuery = "MIN(date) OVER w AS date, id, sensorId, localId, letterCode "
+
+        return when(queryNumber) {
+            FIRST_QUERY, SECOND_QUERY -> "* "
+            THIRD_QUERY, SEVENTH_QUERY -> "AVG(temperature) AS temperature, $pseudonymsStringQuery"
+            FOURTH_QUERY, EIGHTH_QUERY -> "MAX(temperature) AS temperature, $pseudonymsStringQuery"
+            FIFTH_QUERY, NINTH_QUERY -> "MIN(temperature) AS temperature, $pseudonymsStringQuery"
+            SIXTH_QUERY, TENTH_QUERY -> "DISTINCT FIRST_VALUE(temperature) OVER w AS temperature, $pseudonymsStringWindowQuery"
+            else -> "* "
+        }
+    }
+
+    //QUERY STRINGS
+    private fun getQueryWhereMainPartCore(): String {
+        val queryNumber = getQueryNumber()
+        val pseudonymsStringQuery = "MIN(id) AS id, sensorId, localId, letterCode, MIN(date) AS date "
+        val pseudonymsStringWindowQuery = "MIN(date) OVER w AS date, id, sensorId, localId, letterCode "
+
+        return when(queryNumber) {
+            SECOND_QUERY, THIRD_QUERY, FOURTH_QUERY, FIFTH_QUERY, SIXTH_QUERY -> "(sensorId IN ? " +
+                    "OR localId IN ? " +
+                    "OR letterCode IN ?) "
+            else -> ""
+        }
+    }
+
 }
